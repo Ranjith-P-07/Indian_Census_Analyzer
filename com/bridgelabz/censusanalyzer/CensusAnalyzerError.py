c111f536add50b15a85f951151a361fdba43a0fa
Ranjith P <Ranjithroy704@gmail.com>
2020-12-06 00:07:10 +0530

    "Add: created CensusAnalyzerError class to Show Customized Exception"

---
+++ com/bridgelabz/censusanalyzer/CensusAnalyzerError.py
@@ -0,0 +1,8 @@
+# Customized Exception
+class CensusAnalyserError(Exception):
+    def __init__(self, message):
+        super().__init__()
+        self.message = message
+
+    def __str__(self):
+        return self.message
