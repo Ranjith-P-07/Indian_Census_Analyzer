97334782419cf681fde860ce0ae0ab6ccb35f52e
Ranjith P <Ranjithroy704@gmail.com>
2020-12-06 00:52:45 +0530

    "Add: Created StatecodeAnalyzer class to check the Header of the Data"

---
+++ com/bridgelabz/censusanalyzer/StatecodeAnalyzer.py
@@ -0,0 +1,9 @@
+class Statecodeanalyzer:
+    def __init__(self):
+        self.SrNo = "SrNo"
+        self.StateName = "State Name"
+        self.TIN = "TIN"
+        self.StateCode = "StateCode"
+
+    def __repr__(self):
+        return self.SrNo + "," + self.StateName + "," + self.TIN + "," + self.StateCode
