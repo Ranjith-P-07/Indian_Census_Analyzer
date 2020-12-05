2a2ae4cdddc2cf84a0da129a5852fa1435ee811a
Ranjith P <Ranjithroy704@gmail.com>
2020-12-06 00:50:59 +0530

    "Add: Refactored code so that their is only one CSVLoader Class"

--- com/bridgelabz/censusanalyzer/censusanalyzer.py
+++ com/bridgelabz/censusanalyzer/censusanalyzer.py
@@ -1,12 +1,12 @@
 import pandas as pd
 from com.bridgelabz.censusanalyzer.CensusAnalyzerError import CensusAnalyserError
-from com.bridgelabz.censusanalyzer.IndiaCensusCSV import IndiaCensusCSV
 
 
 class CSVLoader:
 
-    def __init__(self, path):
+    def __init__(self, path, opt):
         self.path = path
+        self.opt = opt
 
     def record_counter(self):
         """
@@ -14,10 +14,11 @@ class CSVLoader:
         :return: number of records in file
         """
         try:
-            col_names = repr(IndiaCensusCSV()).split(",")
+            col_names = repr(self.opt).split(",")
             data = pd.read_csv(self.path, usecols=col_names)
             return len(data)
         except FileNotFoundError:
             raise CensusAnalyserError("Check file path")
         except ValueError:
             raise CensusAnalyserError("Wrong Delimiter or Invalid Columns Name")
+
