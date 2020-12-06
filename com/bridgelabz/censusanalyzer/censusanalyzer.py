31df70c10b8cc2d2a1b75001ef810b9e04a39084
Ranjith P <Ranjithroy704@gmail.com>
2020-12-06 23:44:21 +0530

    "Add: Refactored code to make seperate files for CSV loading and for CSV reading"

--- com/bridgelabz/censusanalyzer/censusanalyzer.py
+++ com/bridgelabz/censusanalyzer/censusanalyzer.py
@@ -4,19 +4,19 @@ from com.bridgelabz.censusanalyzer.CensusAnalyzerError import CensusAnalyserErro
 
 class CSVLoader:
 
-    def __init__(self, path, opt):
+    def __init__(self, path, header):
         self.path = path
-        self.opt = opt
+        self.header = header
 
-    def record_counter(self):
+    def csv_loader(self):
         """
         Count record in file
         :return: number of records in file
         """
         try:
-            col_names = repr(self.opt).split(",")
+            col_names = repr(self.header).split(",")
             data = pd.read_csv(self.path, usecols=col_names)
-            return len(data)
+            return data
         except FileNotFoundError:
             raise CensusAnalyserError("Check file path")
         except ValueError:
